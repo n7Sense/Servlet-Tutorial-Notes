02fb62463867501800eac1a669efa8fac5da458d
n7Sense <rajbharrahul1994@gmail.com>
2018-10-16 11:52:19 +0530

    jsp implicit objects

---
+++ jsp/implicit-object.java
@@ -0,0 +1,21 @@
+Implicit Object
+===============
+total 9 implicit object present in jsp
+
+1.request: This is the HttpServletRequest object associated with the request.
+
+2.response: This is the HttpServletResponse object associated with the response to the client.
+
+3.out: This is the PrintWriter object used to send output to the client.
+
+4.session: This is the HttpSession object associated with the request.
+
+5.application: This is the ServletContext object associated with the application context.
+
+6.config: This is the ServletConfig object associated with the page.
+
+7.pageContext: This encapsulates use of server-specific features like higher performance JspWriters.
+
+8.page: This is simply a synonym for this, and is used to call the methods defined by the translated servlet class.
+
+9.Exception: The Exception object allows the exception data to be accessed by designated JSP.
